6a6df0d96692144960f752b37a0ce6af2779bccd
shreyanshrukade-debug <shreyanshrukade@gmail.com>
2026-07-24 07:19:08 +0000

    switch case

--- file1.cpp
+++ file1.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+namespace syc
+{
+int show()
+{
+    std::cout<<"welcome syc";
+    std::cout<<"performe c++ practical";
+    return 0 ;
+}
+}
+
 
--- sur1.cpp
+++ sur1.cpp
@@ -3,48 +3,58 @@ using namespace std;
 
 int main() {
     int a, b, choice;
+    char again;
 
-    cout << "Enter two numbers: ";
-    cin >> a >> b;
+    do {
+        cout << "\nEnter two numbers: ";
+        cin >> a >> b;
 
-    cout << "\n1. Addition";
-    cout << "\n2. Subtraction";
-    cout << "\n3. Multiplication";
-    cout << "\n4. Division";
-    cout << "\n5. Modulus";
-    cout << "\nEnter your choice: ";
-    cin >> choice;
+        cout << "\n===== Calculator Menu =====";
+        cout << "\n1. Addition";
+        cout << "\n2. Subtraction";
+        cout << "\n3. Multiplication";
+        cout << "\n4. Division";
+        cout << "\n5. Modulus";
+        cout << "\nEnter your choice: ";
+        cin >> choice;
 
-    switch(choice) {
-        case 1:
-            cout << "Addition = " << a + b;
-            break;
+        switch(choice) {
+            case 1:
+                cout << "Addition = " << a + b;
+                break;
 
-        case 2:
-            cout << "Subtraction = " << a - b;
-            break;
+            case 2:
+                cout << "Subtraction = " << a - b;
+                break;
 
-        case 3:
-            cout << "Multiplication = " << a * b;
-            break;
+            case 3:
+                cout << "Multiplication = " << a * b;
+                break;
 
-        case 4:
-            if(b != 0)
-                cout << "Division = " << (float)a / b;
-            else
-                cout << "Division by zero is not possible.";
-            break;
+            case 4:
+                if (b != 0)
+                    cout << "Division = " << (float)a / b;
+                else
+                    cout << "Division by zero is not possible.";
+                break;
 
-        case 5:
-            if(b != 0)
-                cout << "Modulus = " << a % b;
-            else
-                cout << "Modulus by zero is not possible.";
-            break;
+            case 5:
+                if (b != 0)
+                    cout << "Modulus = " << a % b;
+                else
+                    cout << "Modulus by zero is not possible.";
+                break;
 
-        default:
-            cout << "Invalid Choice";
-    }
+            default:
+                cout << "Invalid Choice";
+        }
+
+        cout << "\n\nDo you want to perform another calculation? (Y/N): ";
+        cin >> again;
+
+    } while (again == 'Y' || again == 'y');
+
+    cout << "\nThank you for using the calculator!" << endl;
 
     return 0;
 }
